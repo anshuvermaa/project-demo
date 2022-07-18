e43b07a17007785cf7fa03d2d13404169759e403
anshu <lcb2021043@iiitl.ac.in>
2022-07-18 21:12:11 +0530

    commitd

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.get('/api/config/paypal', (req, res) =>
 )
 app.use(cors())
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '/frontend/build/images')))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
